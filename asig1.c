3a8a6b7e8c226b2e2bf71b509509d60e2b06d0c2
Bhushan Pisal <149819594+bhushan620@users.noreply.github.com>
2024-04-22 06:55:48 +0530

    Update asig1.c

--- asig1.c
+++ asig1.c
@@ -2,21 +2,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/types.h>
 
-int main() {
-    printf("__________ OSL Assignment 1 ______________\n");
+int main(){
+    int ch;
     printf("The parent process ID is %d\n", getpid());
 
     printf("\nEnter 1 to create a split process using fork() : ");
     printf("\nEnter 2 to create a split process using fork() with priority execution : ");
-    printf("\nEnter 3 to implement exec() : \n");
-
-    int userChoice;
-    scanf("%d", &userChoice);
-
-    switch (userChoice) {
-        case 1: {
+    printf("\nEnter 3 to implement exec() : ");
+    printf("\nEnter 4 to implement copy command: ");
+    printf("\nEnter 5 to implement search pattern: ");
+    printf("\nEnter 6 to implement exit\n");
+    
+    printf("enter your choice = ");
+    scanf("%d",&ch);
+    
+    
+    
+    switch(ch){
+          case 1: {
             pid_t childPID = fork();
             if (childPID == 0) {
                 printf("We are in the child process. PID = %d \n", getpid());
@@ -45,22 +49,76 @@ int main() {
         case 3: {
             char *args[] = {"./exec", NULL};
             execvp(args[0], args);
+            printf("\nYou are in the original file\n");
             break;
         }
-  
-    }
 
-    printf("\nYou are in the original file\n");
+    	case 4:
+    		copyfile();
+    		break;
 
-    return 0;
+    	case 5:
+			searchpattern();
+			break;
+
+		case 6:
+			printf("= = exiting = =");
+			break;
+
+		default:
+			printf("enter valid number\n");		
+	}
+	return 0;
+	
+}
+
+void copyfile(){
+	pid_t pid = fork();
+	if(pid < 0){
+		printf("fork failed");
+	}
+	else if(pid == 0){
+		char *args[] = {"cp" , "source.txt" , "dest.txt" , NULL};
+		execvp("cp",args);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+	else{
+	    wait(NULL);
+	    printf("file copied");
+	}
+	
+}
+void searchpattern(){
+	char pattern[256];
+	printf("enter pattern to search = ");
+	scanf("%255s",pattern);
+	
+	pid_t pid = fork();
+    
+	if(pid < 0){
+		printf("fork failed");
+	}
+	else if(pid == 0){
+		char *args[] = {"grep" , pattern , "source.txt" , NULL};
+		execvp("grep",args);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+	else{
+	    wait(NULL);
+	    printf("pattern searched");
+	}
+	
 }
 
 
-// exec.c
+
 // #include<stdio.h>
 
 // int main()
 // {
 //   printf("\nThe execvp() system calls has redirected us to the another C file");
 //   printf("\n-------We are in the exec.c file-------\n");
+//   return 0;
 // }
